88c9bb14cc45342722bad5d0bc361133bef8eb90
Juan Pablo Del Greco <juanpablo.delgreco.jp@gmail.com>
2020-05-17 14:15:54 -0300

    Algoritmo realizado
    La entrada se cambiaron los . por , porque da error.
    Se eliminaron y agregaron metodos del diagrama, hay que modificarlo.

--- src/caber/Lanzador.java
+++ src/caber/Lanzador.java
@@ -1,16 +1,19 @@
 package caber;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class Lanzador {
-	private List<Lanzamiento> lanzamientos;
+public class Lanzador implements Comparable<Lanzador>{
+	private int id;
 	private double distTotalRecorrida;
 	private double consistencia;
 	private boolean descalificado;
 
-	public Lanzador(double distTotalRecorrida, double consistencia, boolean descalificado) {
-		lanzamientos = new ArrayList<Lanzamiento>();
+
+	public Lanzador(int id,double distTotalRecorrida, double consistencia, boolean descalificado) {
+		//lanzamientos = new ArrayList<Lanzamiento>();
+		this.id = id;
 		this.distTotalRecorrida = distTotalRecorrida;
 		this.consistencia = consistencia;
 		this.descalificado = descalificado;
@@ -20,17 +23,40 @@ public class Lanzador {
 		return this.descalificado;
 	}
 
-	public int compareDistancia(Lanzador l) {
-		return 0;
+	public int compareConsistencia(Lanzador l) {
+		return (int)this.consistencia - (int)l.consistencia;
 	}
 
-	public int compareConsistencia(Lanzador l) {
-		return 0;
+	public double getDistTotalRecorrida() {
+		return distTotalRecorrida;
+	}
+	
+	public double getConsistencia() {
+		return consistencia;
+	}
+	
+	public int getId() {
+		return id;
 	}
 
 	@Override
 	public String toString() {
-		return "Lanzador [distTotalRecorrida=" + distTotalRecorrida
-				+ ", consistencia=" + consistencia + ", descalificado=" + descalificado + "]";
+		return "Lanzador [id=" + id + ", distTotalRecorrida=" + distTotalRecorrida + ", consistencia=" + consistencia
+				+ ", descalificado=" + descalificado + "]";
+	}
+
+	@Override
+	public int compareTo(Lanzador obj) {
+		return (int)obj.distTotalRecorrida - (int)this.distTotalRecorrida;
+	}
+	
+	public static class Comparators {
+		
+		public static Comparator<Lanzador> CONSISTENCIA = new Comparator<Lanzador>() {
+			@Override
+			public int compare(Lanzador l1, Lanzador l2) {
+				return (int) l1.getConsistencia() - (int) l2.getConsistencia();
+			}
+		};
 	}
 }
--- src/caber/Torneo.java
+++ src/caber/Torneo.java
@@ -2,7 +2,9 @@ package caber;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,14 +25,31 @@ public class Torneo {
 	}
 
 	public void jugarTorneo() {
+		int i;
+		cargarLanzadores();
+		Collections.sort(lanzadores);
+		for (i = 0; i < 3; i++) {
+			podioD.add(lanzadores.get(i));
+		}
+		Collections.sort(lanzadores, Lanzador.Comparators.CONSISTENCIA);
+		i = 0;
+		while (podioC.size() <= 3 && i < lanzadores.size()) {
+			if (!lanzadores.get(i).getDescalificado())
+				podioC.add(lanzadores.get(i));
+			i++;
+		}
+		grabarResultados();
+	}
+
+	public void cargarLanzadores() {
 		try {
 			Scanner sc = new Scanner(new File(inputPath));
 			Lanzador lanzador;
 			Lanzamiento lanzamiento;
 			double distancia = 0;
 			double grados = 0;
-			boolean descalificado  = false;
-			double distanciaTotal= 0, consistencia = 0;
+			boolean descalificado = false;
+			double distanciaTotal = 0, consistencia = 0;
 			cantLanzadores = sc.nextInt();
 			for (int i = 0; i < cantLanzadores; i++) {
 				descalificado = false;
@@ -39,7 +58,7 @@ public class Torneo {
 				for (int j = 0; j < 3; j++) {
 					distancia = sc.nextDouble();
 					grados = sc.nextDouble();
-					lanzamiento = new Lanzamiento(grados,distancia);
+					lanzamiento = new Lanzamiento(grados, distancia);
 					distanciaTotal += lanzamiento.obtenerDistanciaValida();
 					if (lanzamiento.isDescalificado()) {
 						descalificado = true;
@@ -47,24 +66,28 @@ public class Torneo {
 					consistencia += Math.abs(grados);
 				}
 				consistencia /= 3;
-				lanzador = new Lanzador(distanciaTotal, consistencia, descalificado);
+				lanzador = new Lanzador(i + 1, distanciaTotal, consistencia, descalificado);
 				lanzadores.add(lanzador);
 			}
-			for(Lanzador l : lanzadores) {
-				System.out.println(l);
-			}
 			sc.close();
+			System.out.println(outputPath+ " generado con exito!");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-	}
-
-	public void cargarLanzadores() {
-
 	}
 
 	public void grabarResultados() {
+		try {
+			PrintWriter pw = new PrintWriter(new File(outputPath));
+			for (Lanzador l : podioC)
+				pw.print(l.getId() + " ");
+			pw.println();
+			for (Lanzador l : podioD)
+				pw.print(l.getId() + " ");
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
